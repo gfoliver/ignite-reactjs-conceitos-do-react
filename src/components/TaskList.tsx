db73570786b632de8f62e1227756619470ac39aa
Guilherme Fleck Oliveira <guilhermefleckoliveira@gmail.com>
2021-04-27 08:25:54 -0300

    Criando, completando e deletando taskts

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,20 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (! newTaskTitle)
+      return;
+
+    setTasks([...tasks, {id: tasks.length + 1, title: newTaskTitle, isComplete: false}]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => task.id === id ? ({...task, isComplete: ! task.isComplete}) : task));
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
